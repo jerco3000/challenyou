b114ad33ec68dfec395cf3a66b4d7a53a3be43b3
Jaime Chereau <jaime@chereau.cl>
2020-11-11 00:54:46 -0300

    Alineacion de logotipo

--- src/router/index.js
+++ src/router/index.js
@@ -16,12 +16,20 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import("../views/About.vue")
+  },
+  {
+    path: "/grid",
+    name: "Grid",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import("../views/Grid.vue")
   }
 ];
 
 const router = new VueRouter({
+  mode: "history",
   routes
 });
 
